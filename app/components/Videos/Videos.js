a105ff3dfa93aebb4d819724e62b9e1199aec69a
Gasser Mamdouh <33136864+gasser-mamdouh@users.noreply.github.com>
2019-04-09 03:09:02 +0200

    Adding dropzone without the final functionality

--- app/components/Videos/Videos.js
+++ app/components/Videos/Videos.js
@@ -2,27 +2,18 @@ import React from 'react';
 import Video from './Video/Video';
 import classes from './Videos.css';
 
-/*
-ToDo:
-*drop zone
-*routing & link
-*preview
-*preprocessing fade cann't point to it
-*lazy loading
-*some styling to make the image look like video ex: play icon
-*change VideoId to actual videoId
-*/
 const videos = () => {
   const allItems = Array(100)
     .fill()
-    .map((_, i) => <Video key={i} videoId={i}/>);
+    .map((_, i) => <Video key={i} videoId={i} />);
   return (
-  <ul className={classes.VideoList}>
-    { allItems }
-    <li className={classes.PlaceHolder}></li>
-    <li className={classes.PlaceHolder}></li>
-    <li className={classes.PlaceHolder}></li>
-  </ul>);
+    <ul className={classes.VideoList}>
+      {allItems}
+      <li className={classes.PlaceHolder} />
+      <li className={classes.PlaceHolder} />
+      <li className={classes.PlaceHolder} />
+    </ul>
+  );
 };
 
 export default videos;
