5b39f621198abb7872b4d2a20d3171c2c32a5cb1
Gasser Mamdouh <33136864+gasser-mamdouh@users.noreply.github.com>
2019-04-16 15:27:44 +0200

    Refactoring VideoPreview and handling uncaught error

--- app/components/VideoThumbnails/VideoThumbnail/VideoPreview/VideoPreview.js
+++ app/components/VideoThumbnails/VideoThumbnail/VideoPreview/VideoPreview.js
@@ -1,27 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './VideoPreview.css';
-let refs;
 
-const playVideo = () => {
-  refs.play();
-};
+type Props = {};
+class VideoPreview extends Component<Props> {
+  props: Props;
+  constructor(props) {
+    super(props);
+    this.videoRef = React.createRef();
+  }
+  playVideo = () => {
+    if (this.videoRef.currentTime !== 0) {
+      this.videoRef.currentTime = 0;
+    }
+    this.videoRef.play();
+  };
 
-const pauseVideo = () => {
-  refs.pause();
-  refs.currentTime = 0;
-};
+  pauseVideo = () => {
+    if (this.videoRef.currentTime !== 0) {
+      this.videoRef.pause();
+    }
+    this.videoRef.currentTime = 0;
+  };
+  render() {
+    return (
+      <video
+        ref={curRef => (this.videoRef = curRef)}
+        onMouseLeave={this.pauseVideo}
+        onMouseOver={this.playVideo}
+        muted
+      >
+        <source src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4" />
+      </video>
+    );
+  }
+}
 
-const videoPreview = () => {
-  return (
-    <video
-      ref={ref => (refs = ref)}
-      onMouseLeave={pauseVideo}
-      onMouseOver={playVideo}
-      muted
-    >
-      <source src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4" />
-    </video>
-  );
-};
-
-export default videoPreview;
+export default VideoPreview;
