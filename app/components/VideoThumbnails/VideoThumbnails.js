6b255aa20dc193202200289c351b5ce975a00877
OmarBahi <omar.bahi95@gmail.com>
2019-05-17 01:49:20 +0200

    Fixing UI bug

--- app/components/VideoThumbnails/VideoThumbnails.js
+++ app/components/VideoThumbnails/VideoThumbnails.js
@@ -3,12 +3,13 @@ import VideoThumbnail from './VideoThumbnail/VideoThumbnail';
 import classes from './Videothumbnails.css';
 
 const videothumbnails = () => {
-  const allItems = Array(20)
+  const allItems = Array(21)
     .fill()
     .map((_, i) => <VideoThumbnail key={i} videoId={i} ready />);
   return (
     <ul className={classes.VideoList}>
       {allItems}
+      <li className={classes.PlaceHolder} />
       <li className={classes.PlaceHolder} />
       <li className={classes.PlaceHolder} />
       <li className={classes.PlaceHolder} />
