f04b1e2db5f5f6ad041945d39958a2f59a42aa3e
OmarBahi <omar.bahi95@gmail.com>
2019-05-17 02:04:05 +0200

    Unifying components for multiple views

--- app/components/Persons/Persons.js
+++ app/components/Persons/Persons.js
@@ -3,12 +3,23 @@ import Person from './Person/Person';
 import classes from './Persons.css';
 
 const Persons = () => {
-  const allItems = Array(10)
+  const allItems = Array(32)
     .fill()
     .map((_, i) => (
       <Person key={i} personId={i} personName={'Nicholas Cage'} />
     ));
-  return <ul className={classes.PersonsList}>{allItems}</ul>;
+  return <ul className={classes.PersonsList}>
+  {allItems}
+  <li className={classes.PlaceHolder} />
+      <li className={classes.PlaceHolder} />
+      <li className={classes.PlaceHolder} />
+      <li className={classes.PlaceHolder} />
+      <li className={classes.PlaceHolder} />
+      <li className={classes.PlaceHolder} />
+      <li className={classes.PlaceHolder} />
+      <li className={classes.PlaceHolder} />
+      <li className={classes.PlaceHolder} />
+  </ul>;
 };
 
 export default Persons;
