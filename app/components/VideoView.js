53869df7dea358703136ecf196be4788fce0ca01
OmarBahi <omar.bahi95@gmail.com>
2019-06-10 19:49:16 +0200

    Adding video player & solving gasser's bug

--- app/components/VideoView.js
+++ app/components/VideoView.js
@@ -7,6 +7,9 @@ import ToolBar from './ToolBar/ToolBar';
 import classes from './VideoView.css';
 import Title from './Title/Title';
 import Tags from './Tags';
+import { Player,BigPlayButton } from 'video-react';
+
+
 
 export default class VideoView extends Component<Props> {
   props: Props;
@@ -23,7 +26,12 @@ export default class VideoView extends Component<Props> {
         </div>
 
         <div className={classes.VideoDiv}>
-          <img width="95%" src="https://via.placeholder.com/600x360" />
+        <Player >
+          <source src="D:\gradproject\vynd_frontend\app\components\mp4C.mp4" />
+          <BigPlayButton position="center" />
+         </Player>
+          
+
           <Tags />
           <h5>Video ID:{this.props.videoID}</h5>
         </div>
